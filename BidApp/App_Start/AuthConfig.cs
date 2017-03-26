da76b53f0530e8612a6d2d4eab909afc0856ca4d
numidux <takagi1992@gmail.com>
2017-03-26 13:20:48 +0100

    First Commit

---
+++ BidApp/App_Start/AuthConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+
+[assembly: OwinStartup(typeof(BidApp.AuthConfig))]
+namespace BidApp
+{
+    public class AuthConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Authentication/Login")
+            });
+        }
+    }
+}
---
+++ BidApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace BidApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
