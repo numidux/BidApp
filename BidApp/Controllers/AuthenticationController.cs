efb4842ce4caa9989985ab6bbb11ae4327a05d0b
numidux <takagi1992@gmail.com>
2017-03-27 22:19:03 +0100

    Add Claim based authentication Some visual enhancement

--- BidApp/Controllers/AuthenticationController.cs
+++ BidApp/Controllers/AuthenticationController.cs
@@ -1,15 +1,68 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using BidApp.Models;
+using Microsoft.AspNet.Identity;
 
 namespace BidApp.Controllers
 {
     public class AuthenticationController : Controller
     {
-        [HttpPost]
+        public object DefaultAuthentication { get; private set; }
+
         [AllowAnonymous]
-        [ValidateAntiForgeryToken]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model, string returnUri)
+        {
+            ViewBag.ReturnUrl = returnUri;
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if(!ValidateUser(model.Login, model.Password))
+            {
+                ModelState.AddModelError(string.Empty, "Login ou password incorrect");
+                return View(model);
+            }
+            
+            var loginClaim = new Claim(ClaimTypes.NameIdentifier, model.Login);
+            var claimIdentity = new ClaimsIdentity(new[] { loginClaim }, DefaultAuthenticationTypes.ApplicationCookie);
+            var context = Request.GetOwinContext();
+            var authenticationManager = context.Authentication;
+            authenticationManager.SignIn(claimIdentity);
+
+            if (Url.IsLocalUrl(ViewBag.ReturnUrl))
+                return ViewBag.ReturnUrl;
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            var context = Request.GetOwinContext();
+            var authenticationManager = context.Authentication;
+            authenticationManager.SignOut();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool ValidateUser(string login, string password)
+        {
+            if (login.Length != 0 && password.Equals("password"))
+                return true;
+            return false;
         }
     }
 }
