9d64c441bb68ee2309b14125fd1578108b407ed9
numidux <takagi1992@gmail.com>
2017-03-28 23:16:45 +0100

    Add Products MVC components

---
+++ BidApp/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BidApp.Models;
+
+namespace BidApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var firstProduct = new Product(1, "PES 2017", 20);
+            var secondProduct = new Product(2, "FIFA 17", 22);
+
+            var products = new List<Product>();
+            products.Add(firstProduct);
+            products.Add(secondProduct);
+
+            var model = new ProductsViewModel
+            {
+                Products = products
+            };
+
+            return View("List", model);
+        }
+    }
+}
---
+++ BidApp/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BidApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
